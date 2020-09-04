c31766ab91dac2d7a7edea8229bf0536894740e0
Breu Webber <hermesmesquita@gmail.com>
2020-09-04 15:46:11 -0300

    Create primos.js
    procurando ajuda

---
+++ primos.js
@@ -0,0 +1,25 @@
+function buscaNumeroPrimos(n) {
+//não altere a linha acima
+
+/*
+ * Nas linhas a seguir, escreva um código que encontre todos
+ * os números primos entre 0 e n e coloque-os num array chamado
+ * `numerosPrimos`(você deve criar esse array)
+ */
+    var numerosPrimos=[]; 
+    
+    
+    for(var i=numerosPrimos.length;i<n;i++){
+        if (i===2){
+            numerosPrimos.push(i);
+        };
+        if (i=== --numerosPrimos){
+            numerosPrimos.push(i);
+        };
+    }
+    
+    
+
+ //não altere as linhas abaixo
+  return numerosPrimos;
+}
